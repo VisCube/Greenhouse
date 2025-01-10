d12f2f2d961ecb2a48b9159c0062554183d787fe
Mark <180091136+VisCube@users.noreply.github.com>
2025-01-10 21:46:24 +0500

    Refactor main.cpp

--- main.cpp
+++ main.cpp
@@ -10,16 +10,21 @@
 
 /* Входы/выходы компонентов */
 #define PIN_LIGHT A0
-#define PIN_MOIST A1
-#define PIN_TEMPERATURE A2
+#define PIN_TEMPERATURE A1
+#define PIN_MOISTURE A2
 #define PIN_LAMP 2
-#define PIN_SHOWER 3
-#define PIN_COOLER 4
-#define PIN_HEATER 5
+#define PIN_COOLER 3
+#define PIN_HEATER 4
+#define PIN_SHOWER 5
 
-/* Данные для передачи данных на сервер */
-#define SERVER_ADDRESS "..."
-#define SERVER_PORT ...
+/* Шаблоны дисплея */
+#define MESSAGE_READING "%d / %d %s"
+#define MESSAGE_WIFI "WiFi: %s"
+#define MESSAGE_BLE "BLE: %s"
+
+/* Данные для связи с сервером */
+#define SERVER_ADDRESS "metallolom.pythonanywhere.com"
+#define SERVER_PORT 80
 #define SERVER_ENDPOINT_DEVICES "/api/devices/"
 #define SERVER_ENDPOINT_SENSORS "/api/sensors/%d/"
 #define SERVER_ENDPOINT_ACTUATORS "/api/actuators/%d/"
@@ -27,27 +32,30 @@
 
 /* Интервалы циклов */
 #define INTERVAL_READINGS 1000L
-#define INTERVAL_SERVER 60000L
+#define INTERVAL_SERVER 30000L
 #define INTERVAL_BLUETOOTH 0L
-#define INTERVAL_DISPLAY 5000L
+#define INTERVAL_DISPLAY 1000L
 
-/* Заготовки для дисплея */
-#define MESSAGE_READING "%d / %d %s"
-#define MESSAGE_WIFI "WiFi: %s"
-#define MESSAGE_BLE "BLE: %s"
 
-/* Текущие показатели */
-struct Readings {
-    int light; // %
-    int moist; // %
-    int temperature; // °C
+/* Сенсоры */
+struct Sensor {
+    int pin; // Пин для чтения
+    int reading{}; // Текущее значение
+    int reference{}; // Эталонное значение
+    int id{}; // Идентификатор на сервере
+
+    explicit Sensor(const int pin): pin(pin) {
+    }
 };
 
-/* Эталонные значения */
-struct References {
-    int light;
-    int moist;
-    int temperature;
+/* Актуаторы */
+struct Actuator {
+    int pin; // Управляющий пин
+    bool status{}; // Текущее состояние (вкл/выкл)
+    int id{}; // Идентификатор на сервере
+
+    explicit Actuator(const int pin) : pin(pin) {
+    }
 };
 
 /* Данные для подключения к WiFi */
@@ -56,12 +64,11 @@ struct WiFiCon {
     const char *pass; // Пароль
 };
 
-/* Идентификаторы компонентов на сервере */
-struct IDs {
-    int device; // Теплица
-    int light, moist, temperature; // Сенсоры
-    int lamp, shower, cooler, heater; // Актуаторы
-};
+
+// Датчик света, датчик температуры, датчик влажности почвы
+Sensor light(PIN_LIGHT), temperature(PIN_TEMPERATURE), moisture(PIN_MOISTURE);
+// Лампа, кулер, нагреватель, помпа для полива
+Actuator lamp(PIN_LAMP), cooler(PIN_COOLER), heater(PIN_HEATER), shower(PIN_SHOWER);
 
 /* Объекты для работы с датчиком температуры */
 OneWire oneWire(PIN_TEMPERATURE);
@@ -69,27 +76,31 @@ DallasTemperature temperatureSensor(&oneWire);
 
 /* Объекты для работы с дисплеем */
 LiquidCrystal_I2C lcd(0x27, 32, 2);
-int displayState = 0;
-
-/* Объекты для работы с WiFi модулем */
-WiFiClient wifiClient;
-HttpClient httpClient(wifiClient, SERVER_ADDRESS, SERVER_PORT);
+int displayState;
 
 /* Объекты для работы с Bluetooth модулем */
 BLEService ledService("180A");
 BLEStringCharacteristic switchCharacteristic("2A57", BLERead | BLEWrite, 100);
 BLEDevice central;
 
-/* Объекты в памяти */
-Readings readings;
-References references;
+/* Объекты для работы с WiFi модулем */
+WiFiClient wifiClient;
+HttpClient httpClient(wifiClient, SERVER_ADDRESS, SERVER_PORT);
 WiFiCon wifiCon;
-IDs ids;
-int messagesCounter;
-FlashStorage(referencesStorage, References);
+int deviceID;
+int messageCounter;
+
+/* Объекты для работы с памятью */
+FlashStorage(lightStorage, Sensor);
+FlashStorage(temperatureStorage, Sensor);
+FlashStorage(moistureStorage, Sensor);
+FlashStorage(lampStorage, Actuator);
+FlashStorage(coolerStorage, Actuator);
+FlashStorage(heaterStorage, Actuator);
+FlashStorage(showerStorage, Actuator);
 FlashStorage(wifiStorage, WiFiCon);
-FlashStorage(idsStorage, IDs);
-FlashStorage(messagesStorage, int);
+FlashStorage(deviceStorage, int);
+FlashStorage(messageStorage, int);
 
 /* Счётчики циклов */
 unsigned long lastReadings;
@@ -97,49 +108,65 @@ unsigned long lastServer;
 unsigned long lastBluetooth;
 unsigned long lastDisplay;
 
+
 /* Считывает показания датчиков */
 void readSensors() {
-    readings.light = !digitalRead(PIN_LIGHT) * 37 + 42;
-    readings.moist = map(1023 - analogRead(PIN_MOIST), 0, 1023, 0, 100);
     temperatureSensor.requestTemperatures();
-    readings.temperature = static_cast<int>(temperatureSensor.getTempCByIndex(0));
-}
 
-/* Сверяет показания датчиков с эталонными */
-void checkActuators() {
-    /* Добавочные значения - погрешность, чтоб не удерживать точное значение */
-    digitalWrite(PIN_LAMP, readings.light < references.light - 10);
-    digitalWrite(PIN_SHOWER, readings.moist < references.moist - 10);
-    digitalWrite(PIN_COOLER, readings.temperature > references.temperature + 5);
-    digitalWrite(PIN_HEATER, readings.temperature < references.temperature - 5);
+    light.reading = !digitalRead(light.pin) * 37 + 42;
+    temperature.reading = static_cast<int>(temperatureSensor.getTempCByIndex(0));
+    moisture.reading = map(1023 - analogRead(moisture.pin), 0, 1023, 0, 100);
 }
 
 /* Проверяет адекватность эталонных показаний */
 void checkReferences() {
-    const bool shouldSave = references.light < 0 || references.light > 80 ||
-                            references.moist < 0 || references.moist > 80 ||
-                            references.temperature < 10 || references.temperature > 40;
-    if (references.light < 0) references.light = 0; // Отрицательное освещение
-    if (references.light > 80) references.light = 80; // Слишком яркая лампа
-    if (references.moist < 0) references.moist = 0; // Отрицательная влажность
-    if (references.moist > 80) references.moist = 80; // Слишком влажная почва
-    if (references.temperature < 10) references.temperature = 10; // Слишком холодно
-    if (references.temperature > 40) references.temperature = 40; // Слишком жарко
-    if (shouldSave) referencesStorage.write(references);
+    if (light.reference < 0) {
+        // Слишком темно
+        light.reference = 0;
+        lightStorage.write(light);
+    } else if (light.reference > 1000) {
+        // Слишком светло
+        light.reference = 1000;
+        lightStorage.write(light);
+    }
+
+    if (temperature.reference < 10) {
+        // Слишком холодно
+        temperature.reference = 10;
+        temperatureStorage.write(temperature);
+    } else if (temperature.reference > 40) {
+        // Слишком жарко
+        temperature.reference = 40;
+        temperatureStorage.write(temperature);
+    }
+
+    if (moisture.reference < 0) {
+        // Слишком сухо
+        moisture.reference = 0;
+        moistureStorage.write(moisture);
+    } else if (moisture.reference > 70) {
+        // Слишком влажно
+        moisture.reference = 70;
+        moistureStorage.write(moisture);
+    }
 }
 
-/* Выводит строку на дисплей */
-void displayInfo(const String &line) {
-    Serial.println(line);
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print(line);
+/* Сверяет показания датчиков с эталонными */
+void useActuators() {
+    /* Добавочные значения - погрешность, чтоб не удерживать точное значение */
+    lamp.status = light.reading < light.reference - 100;
+    cooler.status = temperature.reading < temperature.reference - 5;
+    heater.status = temperature.reading > temperature.reference + 5;
+    shower.status = moisture.reading < moisture.reference - 10;
+
+    digitalWrite(lamp.pin, lamp.status);
+    digitalWrite(cooler.pin, cooler.status);
+    digitalWrite(heater.pin, heater.status);
+    digitalWrite(shower.pin, shower.status);
 }
 
 /* Выводит две строки на дисплей */
-void displayInfo(char line1[], char line2[]) {
-    Serial.println(line1);
-    Serial.println(line2);
+void displayData(const char *line1, const char *line2) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(line1);
@@ -147,31 +174,85 @@ void displayInfo(char line1[], char line2[]) {
     lcd.print(line2);
 }
 
-/* Выводит всю информацию на дисплей */
+/* Итеративно выводит всю информацию на дисплей */
 void displayAll() {
-    char displayBuffer1[50];
-    char displayBuffer2[50];
+    char line1[16], line2[16];
     switch (displayState) {
         case 0:
-            sprintf(displayBuffer1, "Light:");
-            sprintf(displayBuffer2, MESSAGE_READING, readings.light, references.light, "%");
+            sprintf(line1, "Light:");
+            sprintf(line2, "%d / %d %s", light.reading, light.reference, "Lx");
             break;
         case 1:
-            sprintf(displayBuffer1, "Moisture:");
-            sprintf(displayBuffer2, MESSAGE_READING, readings.moist, references.moist, "%");
+            sprintf(line1, "Temperature:");
+            sprintf(line2, "%d / %d %s", temperature.reading, temperature.reference, "C");
             break;
         case 2:
-            sprintf(displayBuffer1, "Temperature:");
-            sprintf(displayBuffer2, MESSAGE_READING, readings.temperature, references.temperature, "C");
+            sprintf(line1, "Moisture:");
+            sprintf(line2, "%d / %d %s", moisture.reading, moisture.reference, "%");
             break;
         case 3:
-            sprintf(displayBuffer1, MESSAGE_WIFI, WiFi.status() == WL_CONNECTED ? "+" : "-");
-            sprintf(displayBuffer2, MESSAGE_BLE, BLE.connected() ? "+" : "-");
+            sprintf(line1, MESSAGE_WIFI, WiFi.status() == WL_CONNECTED ? "+" : "-");
+            sprintf(line2, MESSAGE_BLE, BLE.connected() ? "+" : "-");
             break;
         default: ;
     }
-    displayInfo(displayBuffer1, displayBuffer2);
     displayState = (displayState + 1) % 4;
+    displayData(line1, line2);
+}
+
+/* Парсит команду и выполняет её */
+void parseMessage(JSONVar message) {
+    const String command = message["command"];
+    if (command == "setReference") {
+        const String sensor = message["sensor"];
+        const int value = message["value"];
+        switch (sensor) {
+            case "light": {
+                light.reference = value;
+                lightStorage.write(light);
+                break;
+            }
+            case "temperature": {
+                temperature.reference = value;
+                temperatureStorage.write(temperature);
+                break;
+            }
+            case "moisture": {
+                moisture.reference = value;
+                moistureStorage.write(moisture);
+                break;
+            }
+            default: ;
+        }
+    }
+    if (command == "setWiFi") {
+        wifiCon.ssid = message["ssid"];
+        wifiCon.pass = message["pass"];
+        wifiStorage.write(wifiCon);
+    }
+    // TODO
+}
+
+/* Включает Bluetooth */
+void initBluetooth() {
+    if (!BLE.begin()) return;
+    BLE.setLocalName("Nano 33 IoT");
+    BLE.setAdvertisedService(ledService);
+    ledService.addCharacteristic(switchCharacteristic);
+    BLE.addService(ledService);
+    BLE.advertise();
+}
+
+/* Получает данные по Bluetooth */
+void pollBluetooth() {
+    central = BLE.central();
+    if (central) {
+        while (central.connected()) {
+            if (switchCharacteristic.written()) {
+                parseMessage(JSON.parse(switchCharacteristic.value()));
+            }
+        }
+    }
 }
 
 /* Поддерживает подключение к WiFi */
@@ -199,30 +280,54 @@ int registerDevice() {
 /* Регистрирует сенсор/актуатор на сервере и возвращает его идентификатор */
 int registerComponent(const char *endpoint, const char *type) {
     JSONVar requestJSON;
-    requestJSON["device"] = ids.device;
+    requestJSON["device"] = deviceID;
     requestJSON["type"] = type;
     JSONVar responseJSON = post(endpoint, requestJSON);
     return responseJSON["id"];
 }
 
+
 /* Регистрирует устройство и все компоненты на сервере и сохраняет их в памяти */
 void checkRegistry() {
-    const bool shouldSave = !(ids.device && ids.light && ids.moist && ids.temperature &&
-                              ids.lamp && ids.shower && ids.cooler && ids.heater);
-    if (!ids.device) ids.device = registerDevice();
-    if (!ids.light) ids.light = registerComponent(SERVER_ENDPOINT_SENSORS, "light");
-    if (!ids.moist) ids.moist = registerComponent(SERVER_ENDPOINT_SENSORS, "moist");
-    if (!ids.temperature) ids.temperature = registerComponent(SERVER_ENDPOINT_SENSORS, "temperature");
-    if (!ids.lamp) ids.lamp = registerComponent(SERVER_ENDPOINT_ACTUATORS, "lamp");
-    if (!ids.shower) ids.shower = registerComponent(SERVER_ENDPOINT_ACTUATORS, "shower");
-    if (!ids.cooler) ids.cooler = registerComponent(SERVER_ENDPOINT_ACTUATORS, "cooler");
-    if (!ids.heater) ids.heater = registerComponent(SERVER_ENDPOINT_ACTUATORS, "heater");
-    if (shouldSave) idsStorage.write(ids);
+    if (!deviceID) {
+        deviceID = registerDevice();
+        deviceStorage.write(deviceID);
+    }
+
+    if (!light.id) {
+        light.id = registerComponent(SERVER_ENDPOINT_SENSORS, "light");
+        lightStorage.write(light);
+    }
+    if (!temperature.id) {
+        temperature.id = registerComponent(SERVER_ENDPOINT_SENSORS, "temperature");
+        temperatureStorage.write(temperature);
+    }
+    if (!moisture.id) {
+        moisture.id = registerComponent(SERVER_ENDPOINT_SENSORS, "moisture");
+        moistureStorage.write(moisture);
+    }
+
+    if (!lamp.id) {
+        lamp.id = registerComponent(SERVER_ENDPOINT_ACTUATORS, "lamp");
+        lampStorage.write(lamp);
+    }
+    if (!cooler.id) {
+        cooler.id = registerComponent(SERVER_ENDPOINT_ACTUATORS, "cooler");
+        coolerStorage.write(cooler);
+    }
+    if (!heater.id) {
+        heater.id = registerComponent(SERVER_ENDPOINT_ACTUATORS, "heater");
+        heaterStorage.write(heater);
+    }
+    if (!shower.id) {
+        shower.id = registerComponent(SERVER_ENDPOINT_ACTUATORS, "shower");
+        showerStorage.write(shower);
+    }
 }
 
 /* Отправляет сведения о сенсоре на сервер */
 void patchSensor(const int id, const int reading, const int reference) {
-    char endpointBuffer[100];
+    char endpointBuffer[64];
     sprintf(endpointBuffer, SERVER_ENDPOINT_SENSORS, id);
 
     JSONVar requestJSON;
@@ -249,47 +354,19 @@ void patchActuator(const int id, const bool status) {
 
 /* Отправляет сведения о всех компонентах на сервер */
 void patchAll() {
-    patchSensor(ids.light, readings.light, references.light);
-    patchSensor(ids.moist, readings.moist, references.moist);
-    patchSensor(ids.temperature, readings.temperature, references.temperature);
-    patchActuator(ids.lamp, readings.light < references.light - 10);
-    patchActuator(ids.shower, readings.moist < references.moist - 10);
-    patchActuator(ids.cooler, readings.temperature > references.temperature + 5);
-    patchActuator(ids.heater, readings.temperature < references.temperature - 5);
-}
-
-/* Включает Bluetooth */
-void initBluetooth() {
-    if (!BLE.begin()) return;
-    BLE.setLocalName("Nano 33 IoT");
-    BLE.setAdvertisedService(ledService);
-    ledService.addCharacteristic(switchCharacteristic);
-    BLE.addService(ledService);
-    BLE.advertise();
-}
-
-/* Парсит команду и выполняет её */
-void parseMessage(JSONVar message) {
-    const String command = message["command"];
-    if (command == "setReference") {
-        const String sensor = message["sensor"];
-        const int value = message["value"];
-        if (sensor == "light") references.light = value;
-        if (sensor == "moist") references.moist = value;
-        if (sensor == "temperature") references.temperature = value;
-        referencesStorage.write(references);
-    }
-    if (command == "setWiFi") {
-        wifiCon.ssid = message["ssid"];
-        wifiCon.pass = message["pass"];
-        wifiStorage.write(wifiCon);
-    }
+    patchSensor(light.id, light.reading, light.reference);
+    patchSensor(moisture.id, moisture.reading, moisture.reference);
+    patchSensor(temperature.id, temperature.reading, temperature.reference);
+    patchActuator(lamp.id, lamp.status);
+    patchActuator(cooler.id, cooler.status);
+    patchActuator(heater.id, heater.status);
+    patchActuator(shower.id, shower.status);
 }
 
 /* Получает новые команды с сервера */
 void pollServer() {
-    char endpointBuffer[100];
-    sprintf(endpointBuffer, SERVER_ENDPOINT_MESSAGES, ids.device, messagesCounter);
+    char endpointBuffer[64];
+    sprintf(endpointBuffer, SERVER_ENDPOINT_MESSAGES, deviceID, messageCounter);
 
     httpClient.get(endpointBuffer);
     const String responseBody = httpClient.responseBody();
@@ -298,62 +375,58 @@ void pollServer() {
     const int newMessages = responseJSON["count"];
     for (int i = 0; i < newMessages; i++)
         parseMessage(responseJSON["messages"][i]["payload"]);
-    messagesCounter += newMessages;
-    if (newMessages) messagesStorage.write(messagesCounter);
-}
-
-void pollBluetooth() {
-    central = BLE.central();
-    if (central) {
-        while (central.connected()) {
-            if (switchCharacteristic.written()) {
-                parseMessage(JSON.parse(switchCharacteristic.value()));
-            }
-        }
-    }
+    messageCounter += newMessages;
+    if (newMessages) messageStorage.write(messageCounter);
 }
 
 void setup() {
     Serial.begin(9600);
     while (!Serial) {
     }
-    lcd.init();
-    lcd.backlight();
-    displayInfo("Initializing...");
 
     pinMode(PIN_LIGHT, INPUT);
-    pinMode(PIN_MOIST, INPUT);
     pinMode(PIN_TEMPERATURE, INPUT);
+    pinMode(PIN_MOISTURE, INPUT);
     temperatureSensor.begin();
+
     pinMode(PIN_LAMP, OUTPUT);
     pinMode(PIN_SHOWER, OUTPUT);
     pinMode(PIN_COOLER, OUTPUT);
     pinMode(PIN_HEATER, OUTPUT);
 
-    references = referencesStorage.read();
+    lcd.init();
+    lcd.backlight();
+    displayData("Initializing...", "");
+
+    light = lightStorage.read();
+    temperature = temperatureStorage.read();
+    moisture = moistureStorage.read();
+    lamp = lampStorage.read();
+    cooler = coolerStorage.read();
+    heater = heaterStorage.read();
+    shower = showerStorage.read();
     wifiCon = wifiStorage.read();
-    ids = idsStorage.read();
-    messagesCounter = messagesStorage.read();
+    messageCounter = messageStorage.read();
 
     initBluetooth();
 }
 
-int iterCounter = 0;
 void loop() {
     readSensors();
     checkReferences();
-    checkActuators();
+    useActuators();
 
-    if (checkWiFi() && iterCounter++ == 10) {
-        displayInfo("Sending data...");
+    pollBluetooth();
+    if (checkWiFi()) {
+        displayData("Sending data...", "");
         checkRegistry();
         patchAll();
+        displayData("", "Data sent.");
+        displayData("Receiving data...", "");
         pollServer();
-        displayInfo("Data received.");
-        iterCounter = 0;
+        displayData("", "Data received.");
     }
-    pollBluetooth();
 
     displayAll();
-    delay(1000);
+    delay(10000);
 }
